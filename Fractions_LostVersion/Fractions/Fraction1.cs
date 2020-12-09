899d0dc40b545e2e2326ec8c0675c4c94780ffee
MaximN54 <75565758+MaximN54@users.noreply.github.com>
2020-12-09 14:35:33 +0700

    Add files via upload

---
+++ Fractions_LostVersion/Fractions/Fraction1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fractions 
+{
+    class Fraction1 
+    {
+        //private int _denumerator;
+        public int Denumerator { get; private set; }
+        public int Numerator { get; private set; }
+        public string OperationName { get; private set; }
+        private Fraction1 _result;
+        public Fraction1 getResult() => _result;
+        private Fraction1() { }
+        public Fraction1(int numerator, int denumerator)
+        {
+            if (numerator < 0 && denumerator < 0||denumerator<0)
+            {
+                numerator *= -1;
+                denumerator *= -1;
+            }
+   
+            Numerator = numerator;
+            if (denumerator != 0)
+                Denumerator = denumerator;
+            else
+                throw new ArgumentOutOfRangeException("На ноль делить нельзя");
+        }
+        public override string ToString() => $"{Numerator}/{Denumerator}";
+        public Fraction1 Sum(Fraction1 f1, Fraction1 f2)
+        {
+            OperationName = "Сумма";
+            _result = new Fraction1();
+            //проверяю эквивалентность знаменателей
+            if (f1.Denumerator == f2.Denumerator)//если одинаковые
+            {
+                _result.Numerator = f1.Numerator + f2.Numerator;
+                _result.Denumerator = f1.Denumerator;
+                if (_result.Numerator == 0)
+                    _result.Denumerator = _result.Numerator / _result.Denumerator;
+                else if (_result.Numerator == 1)
+                    NOD = 1;
+                else
+                {
+                    NOD = _result.GCD(_result.Numerator, _result.Denumerator); ;
+                    _result.Numerator /= NOD;
+                    _result.Denumerator /= NOD;
+                }
+            }
+            else
+            {
+                int higest_denumenator = f1.Denumerator;
+                int lowest_denumenator = f2.Denumerator;//допускаю что так, если не так, то далее есть проверка
+                if (higest_denumenator < lowest_denumenator)//проверка если не так
+                {
+                    higest_denumenator = f2.Denumerator;
+                    lowest_denumenator = f1.Denumerator;
+                }
+                //если бОльший зн делится на меньший без остатка
+                if (higest_denumenator % lowest_denumenator == 0)
+                {
+                    //теперь нужно числитель меньшей дроби умножить на результат деления большего знам. на меньший
+                    if (lowest_denumenator == f1.Denumerator)
+                        f1.Numerator *= (higest_denumenator / lowest_denumenator);
+                    else
+                        f2.Numerator *= (higest_denumenator / lowest_denumenator);
+                    _result.Numerator = f1.Numerator + f2.Numerator;
+                    _result.Denumerator = f1.Denumerator;
+                }
+                else//если бОльший не делится на меньший без остатка
+                {
+                    _result.Numerator = (f1.Numerator * f2.Denumerator) + (f2.Numerator * f1.Denumerator);
+                    _result.Denumerator = f1.Denumerator * f2.Denumerator;
+                }
+                if (_result.Numerator == 0)
+                    _result.Denumerator = _result.Numerator / _result.Denumerator;
+                else if (_result.Numerator != 1)
+                    NOD = _result.GCD(_result.Numerator, _result.Denumerator);
+                else
+                    NOD = 1;
+                _result.Numerator /= NOD;
+                _result.Denumerator /= NOD;
+            }
+            return _result;
+        }
+        public Fraction1 Subtruction(Fraction1 f1, Fraction1 f2)
+        {
+            _result = new Fraction1();
+            OperationName = "Вычитание";//нужно найти НОЗ
+            if (f1.Denumerator == f2.Denumerator)//проверяю эквивалентность знаменателей, если равны
+            {
+                _result.Numerator = (f1.Numerator - f2.Numerator);
+                _result.Denumerator = f1.Denumerator;
+                if (_result.Numerator == 0)
+                    _result.Denumerator = _result.Numerator / _result.Denumerator;
+                else if (_result.Numerator != 1)
+                    NOD = GCD(_result.Numerator, _result.Denumerator);
+                else
+                    NOD = 1;
+                if (_result.Numerator != 0)
+                {
+                    _result.Numerator /= NOD;
+                    _result.Denumerator /= NOD;
+                }
+            }
+            else
+            //если знаменатели не равны //нахожу БОЛЬШИЙ и МЕНЬШИЙ знаменатели
+            {
+                int higest_denumenator = f1.Denumerator;
+                int lowest_denumenator = f2.Denumerator;//допускаю что так, если не так, то далее есть проверка
+                if (higest_denumenator < lowest_denumenator)//проверка если не так
+                {
+                    higest_denumenator = f2.Denumerator;
+                    lowest_denumenator = f1.Denumerator;
+                }
+                //если бОльший зн делится на меньший без остатка
+                if (higest_denumenator % lowest_denumenator == 0)
+                {
+                    //теперь нужно числитель меньшей дроби умножить на результат деления большего знам. на меньший
+                    if (lowest_denumenator == f1.Denumerator)
+                        f1.Numerator *= (higest_denumenator / lowest_denumenator);
+                    else
+                        f2.Numerator *= (higest_denumenator / lowest_denumenator);
+                    _result.Numerator = f1.Numerator - f2.Numerator;
+                    _result.Denumerator = f1.Denumerator;
+                }
+                else//если бОльший не делится на меньший без остатка
+                { _result.Numerator = (f1.Numerator * f2.Denumerator) - (f2.Numerator * f1.Denumerator);
+                    _result.Denumerator = f1.Denumerator * f2.Denumerator;
+                }
+                if (_result.Numerator == 0)
+                    _result.Denumerator = _result.Numerator / _result.Denumerator;
+                else if (_result.Numerator != 1)
+                    NOD = _result.GCD(_result.Numerator, _result.Denumerator);
+                else
+                    NOD = 1;
+                _result.Numerator /= NOD;
+                _result.Denumerator /= NOD;
+            }
+            return _result;
+        }
+        public Fraction1 Multiplication(Fraction1 f1, Fraction1 f2)
+        {
+            _result = new Fraction1();
+            OperationName = "Умножение";
+            _result.Numerator = f1.Numerator * f2.Numerator;
+            _result.Denumerator = f1.Denumerator * f2.Denumerator;
+            int NOD = GCD(_result.Numerator, _result.Denumerator);
+            _result.Numerator /= NOD;
+            _result.Denumerator /= NOD;
+
+            return _result;
+        }
+        public Fraction1 Division(Fraction1 f1, Fraction1 f2)
+        {
+            _result = new Fraction1();
+            OperationName = "Деление";
+            _result.Numerator = f1.Numerator * f2.Denumerator;
+            _result.Denumerator = f1.Denumerator * f2.Numerator;
+            int NOD = GCD(_result.Numerator, _result.Denumerator);
+            _result.Numerator /= NOD;
+            _result.Denumerator /= NOD;
+
+            return _result;
+        }
+        
+        //ф-ция поиска НОДа
+        private int result, NOD;
+        private int GCD(int num, int denum)
+        {
+            NOD = 0;
+            if (num < 0) num *= -1;
+            if (denum < 0) denum *= -1;
+            if (num < denum)
+            {
+                int c = num;
+                num = denum;
+                denum = c;
+            }
+            while (NOD == 0)
+            {
+                result = num % denum;
+                if (result == 0)
+                    NOD = denum;
+                else
+                {
+                    num = denum;
+                    denum = result;
+                }
+            }
+            return NOD;
+        }
+    }
+}
---
+++ Fractions_LostVersion/Fractions/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fractions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction1 f = new Fraction1(3, 5);
+            Fraction1 f1 = new Fraction1(2, 4);
+            Fraction1 f2 = new Fraction1(-2, 4);
+            Fraction1 f3 = new Fraction1(2, -4);
+            Fraction1 f4 = new Fraction1(-2, -4);
+
+            Console.WriteLine("\nТест Суммирование");
+            PrintResultOperation(f, f1, "+");
+            PrintResultOperation(f, f, "+");
+            PrintResultOperation(f2, f, "+");
+            PrintResultOperation(f3, f4, "+");
+
+            Console.WriteLine("\nТест Вычитание");
+            PrintResultOperation(f, f1, "-");
+            PrintResultOperation(f, f, "-");
+            PrintResultOperation(f2, f, "-");
+            PrintResultOperation(f3, f4, "-");
+
+            Console.WriteLine("\nТест - умножение");
+            PrintResultOperation(f, f1, "*");
+            PrintResultOperation(f, f, "*");
+            PrintResultOperation(f2, f, "*");
+            PrintResultOperation(f3, f4, "*");
+
+            Console.WriteLine("\nТест деление");
+            PrintResultOperation(f, f1, "/");
+            PrintResultOperation(f, f, "/");
+            PrintResultOperation(f2, f, "/");
+            PrintResultOperation(f3, f4, "/");
+
+
+            //Console.WriteLine(0/f4.Denumerator);
+            Console.ReadKey();
+        }
+
+        public static void PrintResultOperation(Fraction1 f1, Fraction1 f2, string operation)
+        {
+            switch(operation)
+            {
+                case "+":f1.Sum(f1, f2);break;
+                case "-":f1.Subtruction(f1,f2);break;
+                case "*":f1.Multiplication(f1,f2);break;
+                case "/":f1.Division(f1,f2);break;
+            }
+            Console.WriteLine($"{f1.OperationName} дробей {f1} и {f2} = {f1.getResult().Numerator}/{f1.getResult().Denumerator}");
+        }
+    }
+}
+/*
+ 1.Прочитать и выучить файл приложения
+2. Написать класс обыкновенной дроби, содержащий следующие поля, доступные для чтения и записи:
+2.1 Числитель дроби
+2.2 Знаменатель дроби
+2.3 Значение дроби типа double
+В сеттере знаменателя дроби обязательно выполнять проверку на 0, при возможности воспользоваться механизмом исключений, а так же 
+класс должен содержать следующие методы:
+2.4 Сложение дробей
+2.5 Вычитание дробей
+2.6 Произведение дробей
+2.7 Частное дроби
+Так же класс дроби должен содержать следующий приватный метод:
+2.8 Сокращение дроби
+Так же класс дроби должен содержать следующие конструкторы:
+2.9 Конструктор без параметров(в нём числителю и знаменателю присваиваются значения равные 1)
+2.10 Конструктор с двумя параметрами( параметры два целых числа - числитель и знаменатель)
+В конструкторе 2.10 так же обязательно реализовать проверку числа на 0, так же желательно использовать механизм исключений. Так же класс 
+должен содержать следующией перегруженный метод:
+2.11 ToString, в котором дробь выводится в виде "{числитель}/{знаменатель}
+При выполнении арифметических операций, конструкторов и изменений числителя или знаменателя дробь обязательно должна сокращаться. Дроби выводятся 
+только в сокращённом виде. Так же крайне важно не допускать по возможности дублирования кода, при черезмерном дублировании кода будут 
+сниматься баллы за работу(то есть возмиожно выделение дополнительных методов или даже классов, не указанных в задании).
+ */
